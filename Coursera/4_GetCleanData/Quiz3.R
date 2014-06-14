ef77a69120f9cd1f83e391d879f49670cab31f40
Shuo Lv <claus.lv@hotmail.com>
2014-06-14 21:08:18 +0800

    Quiz 3 finished

--- Coursera/4_GetCleanData/Quiz3.R
+++ Coursera/4_GetCleanData/Quiz3.R
@@ -30,10 +30,33 @@ url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 f <- file.path(getwd(), "GDP.csv")
 download.file(url, f, method="curl")
 
-dtGDP <- read.csv(f,header=TRUE, sep=",",skip = 5,nrows = 195)
-##dtGDP <- read.xls?
+dtGDP <- data.table(read.csv(f, skip = 4, nrows = 215))
+## dtGDP <- read.csv(f,header=TRUE, sep=",",skip = 4,nrows = 215)
+dtGDP <- dtGDP[X != ""]
+dtGDP <- dtGDP[, list(X, X.1, X.3, X.4)]
+setnames(dtGDP, c("X", "X.1", "X.3", "X.4"), c("CountryCode", "rankingGDP", 
+                                               "Long.Name", "gdp"))
 
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
 f <- file.path(getwd(), "EDUSTATS_Country.csv")
 download.file(url, f, method="curl")
 dtEd <- data.table(read.csv(f))
+
+## join 2 tables
+dt <- merge(dtGDP,dtEd, all = TRUE, by = "CountryCode")
+## remove Null
+dt <- dt[!is.na(dt$rankingGDP),]
+
+## sort
+ordered <- dt[order(rankingGDP, decreasing = TRUE), list(CountryCode, Long.Name.x, Long.Name.y, 
+                                                         rankingGDP, gdp)]
+## 13th item
+ordered[13]
+
+## Question 4
+dt[, mean(rankingGDP, na.rm = TRUE), by = Income.Group]
+
+## Question 5
+breaks <- quantile(dt$rankingGDP, probs = seq(0, 1, 0.2), na.rm = TRUE)
+dt$quantileGDP <- cut(dt$rankingGDP, breaks = breaks)
+dt[Income.Group == "Lower middle income", .N, by = c("Income.Group", "quantileGDP")]
